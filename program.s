37f60a75bdbf8f2d8eb994687003b6e8358fa716
Dmitry Nechaev <fruitpizza49@gmail.com>
2017-10-20 15:30:22 +0300

    Done

--- program.s
+++ program.s
@@ -1,8 +1,27 @@
 section .data
-    format: db "z = %d", 10, 0  ; data bytes z = %d\n\0
+    format: db "Discriminant = %d", 10, 0  ; data bytes z = %d\n\0
 section .text
     global main  ; Export main for linker
     extern printf  ; Request linking with printf in linker
+discr:
+    ;Preamble
+    push ebp
+    mov ebp, esp
+
+    ;Body
+    mov eax, dword [ebp + 12]
+    mul eax
+    mov ecx, eax
+    mov eax, dword [ebp + 8]
+    sal eax, 2
+    mul dword[ebp + 16]
+    neg eax
+    add eax, ecx
+
+    pop ebp
+    ret
+
+
 sum:
     ; Preable
     push ebp
@@ -24,14 +43,15 @@ main:
     push edi
 
     push dword 2  ; y
-    push dword 1  ; x
-    call sum
+    push dword 4  ; x
+    push dword 3
+    call discr
     add esp, 8
 
     push eax  ; result of sum
     push format
     call printf
-    add esp, 8
+    add esp, 12
 
     pop edi
     pop esi
